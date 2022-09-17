ec6f568d69f6e8b26ef23ed20d0c0c4935f3458e
Mohamed Sami <srmf2025@gmail.com>
2022-09-17 23:41:22 +0530

    :sparkles: add api routes & controllers

---
+++ src/api/pets.ts
@@ -0,0 +1,74 @@
+import express from "express";
+import { getDatabase } from "../utils/database";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const db = await getDatabase();
+
+    const data = await db.collection("pets").find().sort({ _id: -1 }).toArray();
+    if (data.length === 0) return res.status(204);
+    res.send(data);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/:_id", async (req, res) => {
+  try {
+    const db = await getDatabase();
+    const _id = parseInt(req.params._id);
+
+    const data = await db.collection("pets").findOne({ _id });
+    if (!data) return res.status(400).send({ _id, message: "Specified _id does not exist" });
+    res.send(data);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const data = req.body;
+  try {
+    const db = await getDatabase();
+
+    await db.collection("pets").insertOne({ ...data });
+    res.send({ message: "Added to the database successfully", data });
+  } catch (error) {
+    res.status(400).send({ error: error, data });
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  let data = req.body;
+  try {
+    const db = await getDatabase();
+    const _id = parseInt(req.params._id);
+
+    await db.collection("pets").updateOne({ _id }, { $set: { ...data } });
+    data = await db.collection("pets").findOne({ _id });
+
+    if(!data) return res.status(400).send({ _id, message: "Specified _id does not exist" })
+    res.send({ message: "Updated data successfully", data });
+  }
+  catch (error) {
+    res.status(400).send({ error: error, data });
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  try {
+    const db = await getDatabase();
+    const _id = parseInt(req.params._id);
+
+    const { deletedCount } = await db.collection("pets").deleteOne({ _id });
+    if (deletedCount === 0) return res.status(400).send({ message: "Specified _id does not exist" });
+    res.send({ _id, message: "Data deleted successfully" });
+  }
+  catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import petsRouter from "./api/pets";
 import { port } from "./utils/config";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ name: "Pet Store API", endpoint: "/api/" });
+  res.send({ name: "Pet Store API", endpoint: "/api/pets" });
 });
+
+app.use("/api/pets", petsRouter);
 
 app.listen(port, () => console.log(`app running at http://localhost:${port}`));
