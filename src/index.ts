0b9b5e3e90578d2c04b60030834985ea777430fb
Mohamed Sami <srmf2025@gmail.com>
2022-09-17 23:44:25 +0530

    :sparkles: add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 import petsRouter from "./api/pets";
 import { port } from "./utils/config";
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
   res.send({ name: "Pet Store API", endpoint: "/api/pets" });
